c56fbb8dc74de9d5330949bfb80d3cfa204f734e
abhimaurya <arm20493@gmail.com>
2020-01-05 19:58:04 +0530

    Updates

---
+++ precache-manifest.aaec8b92e3c5715bf28c179fb8843181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ab822797235d92f54e1f06eba54c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cccf8c0969e09b238a0",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "10aaa18411095c976b0e",
+    "url": "/static/css/main.15364b9b.chunk.css"
+  },
+  {
+    "revision": "5cccf8c0969e09b238a0",
+    "url": "/static/js/2.e136dab9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e136dab9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10aaa18411095c976b0e",
+    "url": "/static/js/main.8e1c3801.chunk.js"
+  },
+  {
+    "revision": "5594771136811fab885e",
+    "url": "/static/js/runtime-main.29ce243d.js"
+  }
+]);
